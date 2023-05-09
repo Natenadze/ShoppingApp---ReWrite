afed283a2c504839b4253e356a86017ab5958e2c
davit <davit.natenadze@gmail.com>
2023-05-09 14:18:58 +0200

    add missing button to result screen

---
+++ FinalShopping/Login+Register/Controller/RegisterVC.swift
@@ -0,0 +1,8 @@
+//
+//  RegisterVC.swift
+//  FinalShopping
+//
+//  Created by Davit Natenadze on 09.05.23.
+//
+
+import Foundation
--- FinalShopping/PaymentResult/PaymentResult.swift
+++ FinalShopping/PaymentResult/PaymentResult.swift
@@ -14,6 +14,7 @@ class PaymentResultVC: UIViewController {
     let stackView = UIStackView()
     let label = UILabel()
     let imageView = UIImageView()
+    let button = UIButton(type: .system)
     
     let image: UIImage
     let message: String
@@ -38,6 +39,11 @@ class PaymentResultVC: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK: - Selector
+    @objc func buttonTapped() {
+        print("Done")
+    }
+    
 }
 
 
@@ -48,15 +54,23 @@ extension PaymentResultVC {
     func style() {
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .vertical
-        stackView.spacing = 20
+        stackView.spacing = 12
         
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.image = image
         
         label.translatesAutoresizingMaskIntoConstraints = false
         label.text = message
+        label.numberOfLines = 0
         label.textAlignment = .center
         label.font = UIFont.preferredFont(forTextStyle: .title1)
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Done", for: .normal)
+        button.tintColor = .white
+        button.layer.cornerRadius = 5
+        button.backgroundColor = .buttonCustomColor
+        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
     }
     
     func layout() {
@@ -64,10 +78,23 @@ extension PaymentResultVC {
         stackView.addArrangedSubview(imageView)
         stackView.addArrangedSubview(label)
         view.addSubview(stackView)
+        view.addSubview(button)
         
         NSLayoutConstraint.activate([
+            // imageView
+            imageView.heightAnchor.constraint(equalToConstant: 200),
+            imageView.widthAnchor.constraint(equalToConstant: 200),
+            
+            // stack
             stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            
+            // button
+            button.heightAnchor.constraint(equalToConstant: 50),
+            button.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 2),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: button.trailingAnchor, multiplier: 2),
+            view.safeAreaLayoutGuide.bottomAnchor.constraint(equalToSystemSpacingBelow: button.bottomAnchor, multiplier: 2)
+            
         ])
     }
 }
